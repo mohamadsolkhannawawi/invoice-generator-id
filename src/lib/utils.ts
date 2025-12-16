8c87f3e793d15869ca9e6053aacf1f811018f028
MohamadSolkhanNawawi <mohamadsolkhan558@gmail.com>
2025-12-16 20:28:19 +0700

    chore(setup): initialize project with next.js, tailwind, and shadcn ui

---
+++ src/lib/utils.ts
@@ -0,0 +1,22 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs));
+}
+
+// FORMATTER KHUSUS INDONESIA
+export const formatRupiah = (amount: number) => {
+    return new Intl.NumberFormat("id-ID", {
+        style: "currency",
+        currency: "IDR",
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0,
+    }).format(amount);
+};
+
+export const formatTanggalIndonesia = (date: Date) => {
+    return new Intl.DateTimeFormat("id-ID", {
+        dateStyle: "long",
+    }).format(date);
+};
